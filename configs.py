3c09b27f9cbc2b76273a72c4fffd46b801ffce63
r2spr <105548769+r2spr@users.noreply.github.com>
2022-06-30 22:53:32 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -19,7 +19,7 @@ class Config(object):
 	BANNED_CHAT_IDS = list(set(int(x) for x in os.environ.get("BANNED_CHAT_IDS", "-1001362659779 -1001255795497").split()))
 	OTHER_USERS_CAN_SAVE_FILE = bool(os.environ.get("OTHER_USERS_CAN_SAVE_FILE", True))
 	ABOUT_BOT_TEXT = f"""
-This is Permanent Files Store Bot!
+This is Permanent @HombaleCinemas Files Store Bot!
 Send me any file I will save it in my Database. Also works for channel. Add me to channel as Admin with Edit Permission, I will add Save Uploaded File in Channel & add Sharable Button Link.
 
 🤖 **My Name:** [HombaleCinemas Bot](https://t.me/{BOT_USERNAME})
